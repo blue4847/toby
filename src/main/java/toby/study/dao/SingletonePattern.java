be37bdf7dbdb96d982b7cb3cc358edc9e19b1739
blue4 <blue4@192.168.0.10>
2016-11-20 00:05:08 +0900

    edit comment

--- src/main/java/toby/study/dao/SingletonePattern.java
+++ src/main/java/toby/study/dao/SingletonePattern.java
@@ -4,12 +4,18 @@ package toby.study.dao;
 public class SingletonePattern {
 	private static SingletonePattern INSTANCE;
 
+	/** private constructor */
 	private SingletonePattern() {
 	}
 
+	/** get single-tone bean instance */
 	public static synchronized SingletonePattern getInstance() {
+		
 		if (INSTANCE == null)
+			// create new instance
 			INSTANCE = new SingletonePattern();
+		
+		// return single-tone instance
 		return INSTANCE;
 	}
 }
